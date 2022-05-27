a529dfe2aa9b0ef5e718ae4e6c854775c5c426f6
Benjamin Nonesoung <nonesoungben@gmail.com>
2022-05-27 13:37:32 -0700

    Add project files.

---
+++ Binary To Decimal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Binary2Decimal
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] binary = { 1,0,0,1,0,1,0,1,1,0 };
+            int[] stored = new int[10];
+            for (int i = 0; i < binary.Length; i++)
+            {
+               int result = 0 * 2 + binary[i];
+                stored[i] = result;
+
+                Console.WriteLine(stored[i]);
+                if (true) {
+                    switch { 
+                        case "0":
+                            0 * 2 + 1;
+                            break;
+                        case "1":
+                            1 * 2 + 1;
+                            break;
+                    }
+                   
+                   
+                }
+            }
+        }
+    }
+}
